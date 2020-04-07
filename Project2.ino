95d59850aceeb64d9cd3d0981e2e139299c9fdc0
ashleywoon <54552775+ashleywoon@users.noreply.github.com>
2020-04-07 06:20:48 -0700

    Finished project upload
    Uploading: updated project2.ino,
    schematic, README.md, jpg overview, jpg closeup, design document

---
+++ Project2.ino
@@ -0,0 +1,228 @@
+/*
+ * Art 385 Project 2
+ * Light Focus
+ * by Ashley Woon
+ * 
+ * Components: Adafruit ESP32 feather, breadboard, connecting wires, (1)RGB LED, (1) push button, 
+ * (1) potentiometer, and (4) resistors.
+ * 
+ * Project 2 is an Arduino sketch incorporating hardware and software 
+ * to create an interactive focus breathing tool. 
+ * 
+ * Toggling between the four states can be done through using the push button. 
+ * The default state allows the user the change the RGB LED output by twisting the potentiometer. 
+
+ */
+
+#include <analogWrite.h>
+//RGB LED pins
+const int redLED = 12;
+const int greenLED = 27;
+const int blueLED = 33;
+int red = 0;
+int green = 0;
+int blue = 0;
+
+//Potentiometer pin
+const int POT = A0;
+int potVal = 0;
+int colorPOT;
+
+//Push button pin and variables
+const int button = A1;
+int buttonState;
+int lastButtonState;
+int pushCount = 0;
+
+//State machine
+int state = 0;
+int stateStressed = 1;
+int stateMeh = 2;
+int stateCalm = 3;
+
+//Timer variables
+unsigned long currentMillis;
+unsigned long prevMillis;
+int i;
+long colorTimer; 
+
+void setup() {
+  Serial.begin(9600);
+  //initializing inputs
+  pinMode(POT, INPUT);
+  pinMode(button, INPUT);
+  //initializing outputs
+  pinMode(redLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+  pinMode(blueLED, OUTPUT);
+  prevMillis = millis();
+}
+
+void loop() {
+  currentMillis = millis();
+  buttonState = digitalRead(button);
+  potVal = analogRead(POT);
+  colorPOT = map(potVal, 0, 4095, 0, 255);
+  colorPOT = constrain(colorPOT, 0, 255);
+  colorTimer = random(0, 10);
+  
+  if(buttonState != lastButtonState) 
+    {
+      if(buttonState == HIGH)
+      {
+        pushCount++;
+      }
+      if(pushCount == 4)
+      {
+        pushCount = 0;
+      }
+    }
+  lastButtonState = buttonState;
+  
+  if(currentMillis >= prevMillis)
+  {
+    changeStates(pushCount);
+  }
+    
+}
+
+void changeStates(int count) {
+  //switching states based on button push
+  if (pushCount == 0)
+  {
+    state = 0;
+    colorful();
+  }
+  else if (pushCount == 1)
+  {
+    state = stateStressed;
+    analogWrite(greenLED, 0);
+    analogWrite(blueLED, 0);
+    stressed();
+  }
+  else if (pushCount == 2)
+  {
+    state = stateMeh;
+    analogWrite(blueLED, 0);
+    meh();
+  }
+  else if (pushCount == 3)
+  {
+   state = stateCalm; 
+   analogWrite(greenLED, 0);
+   analogWrite(redLED, 0);
+   calm();
+  }
+}
+
+void colorful() {
+  i++;
+  if(i < colorTimer)
+  {
+    red+=colorPOT;
+    green+=255-colorPOT;
+    blue+=colorPOT;
+  }
+  else if(i < colorTimer+5)
+  {
+    red++;
+    green--;
+    blue+=colorPOT;
+  }
+  else if(i < colorTimer+7)
+  {
+    red+=255-colorPOT;
+    green+=colorPOT;
+    blue+=255-colorPOT;
+  }
+  analogWrite(redLED, red);
+  analogWrite(greenLED, green);
+  analogWrite(blueLED, blue);
+  prevMillis = millis() + 1000;
+  
+}
+
+void calm() {
+  i ++;
+  //inhale for 4
+  if(i < 5)
+  {
+    blue++;
+  }
+  //hold for 7
+  else if(i > 4 && i < 11)
+  {
+    blue = 80;
+  }
+  //exhale for 8
+  else if(i > 11 && i < 20)
+  {
+    blue-=10;
+  }
+  else if(i >=20)
+  {
+    i = -1;
+    blue = 0;
+  }
+  analogWrite(blueLED, blue);
+  prevMillis = millis() + 1000;
+}
+
+void meh() {
+  i ++;
+  //inhale for 3
+  if(i < 4)
+  {
+    red++;
+    green++;
+  }
+  //hold for 3
+  else if(i > 3 && i < 7 )
+  {
+    red = 50;
+    green = 50;
+  }
+  //exhale for 5
+  else if(i > 6 && i < 12)
+  {
+    red-=10;
+    green-=10;
+  }
+  //repeat
+  else if(i >= 12)
+  {
+    i = -1;
+    red = 0;
+    green = 0;
+  }
+  analogWrite(redLED, red);
+  analogWrite(greenLED, green);
+  prevMillis = millis() + 1000;
+}
+
+void stressed() {
+    i ++;
+  //inhale for 2
+  if(i < 3)
+  {
+    red++;
+  }
+  //hold for 3
+  else if(i > 2 && i < 6 )
+  {
+    red = 30;
+  }
+  //exhale for 4
+  else if(i > 5 && i < 10)
+  {
+    red-=8;
+  }
+  //repeat
+  else if(i >= 10)
+  {
+    i = -1;
+    red = 0;
+  }
+  analogWrite(redLED, red);
+  prevMillis = millis() + 1000;
+}
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # Project2
 ###By Ashley Woon
 
-####An Arduino sketch
+####Project 2 is an Arduino sketch incorporating hardware and software to create an interactive focus breathing tool. 
+####Toggling between the four states can be done through using the push button. 
+The default state allows the user the change the RGB LED output by twisting the potentiometer. 
+
+After the first button push, the red LED will loop through a timed fade that the user can match their breathing to. This red state has the quickest tempo meant to be used when agitated.
+
+The second button push begins the yellow LED timed loop which brightens to a count of 3, stays steady for a count of 3, and fades for a count of 5.
+
+A third button push starts the slowest cycle for a count of 4-7-8.
+
+Another push returns the LED to its default state with a randomized fade loop.
